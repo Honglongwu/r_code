b592f93e61e30fe6a1428a2307e3bdd8ad6b7972
Honglongwu <wuhonglong.china@gmail.com>
2013-03-19 23:21:38 +0800

    Add R_Graphics_Cookbook to my repo

--- plot_line.R
+++ plot_line.R
@@ -5,5 +5,5 @@ df1 <- df1 <- data.frame(sex       = factor(c("Female","Female","Male","Male")),
 library(ggplot2)
 p <- ggplot(data=df1, aes(x = time, y = total_bill, group = sex, colour = sex))
 p + geom_line(aes(linetype=sex), size = 1) + ylim(0, max(df1$total_bill)) + geom_point(size = 3, fill = "white") + scale_colour_hue(name = "Sex of payer", l = 30) +
-  scale_shape_manual(name = "test",values = c(19,22)) + scale_linetype_discrete(name = "Sex of payer") +
+  scale_shape_manual(name = "Sex of payer",values = c(19,22)) + scale_linetype_discrete(name = "Sex of payer") +
   theme_bw() + theme(legend.position=c(.7,.4))
